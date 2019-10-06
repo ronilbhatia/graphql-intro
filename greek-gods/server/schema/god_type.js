9c4708cb19e4166f1685e875535c9c3f7f5e8412
Ronil Bhatia <ronilbhatia@gmail.com>
2019-10-05 21:29:27 -0700

    Finish up GodType to create fully bidirectional GraphQL Schema

--- greek-gods/server/schema/god_type.js
+++ greek-gods/server/schema/god_type.js
@@ -3,6 +3,7 @@ const { GraphQLID, GraphQLString, GraphQLObjectType, GraphQLList } = graphql;
 
 const mongoose = require('mongoose');
 const God = mongoose.model('god');
+const Abode = mongoose.model('abode');
 
 const GodType = new GraphQLObjectType({
   name: 'GodType',
@@ -11,7 +12,41 @@ const GodType = new GraphQLObjectType({
     name: { type: GraphQLString },
     type: { type: GraphQLString },
     description: { type: GraphQLString },
-    domains: { type: new GraphQLList(GraphQLString) }
+    domains: { type: new GraphQLList(GraphQLString) },
+    abode: {
+      type: require('./abode_type'),
+      resolve({ abode }) {
+        return Abode.findById(abode)
+          .then(abode => abode)
+          .catch(err => null)
+      }
+    },
+    emblems: {
+      type: new GraphQLList(require('./emblem_type')),
+      resolve({ id }) {
+        return God.findById(id)
+          .populate('emblems')
+          .then(god => god.emblems)
+      }
+    },
+    parents: {
+      type: new GraphQLList(GodType),
+      resolve({ id }) {
+        return God.findRelatives(id, 'parents')
+      }
+    },
+    children: {
+      type: new GraphQLList(GodType),
+      resolve({ id }) {
+        return God.findRelatives(id, 'children')
+      }
+    },
+    siblings: {
+      type: new GraphQLList(GodType),
+      resolve({ id }) {
+        return God.findRelatives(id, 'siblings')
+      }
+    }
   })
 });
 
